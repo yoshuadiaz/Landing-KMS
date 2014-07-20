8b51eece6792f18e210a41b075a55d4471f46e1d
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-07-20 14:42:11 -0500

    Rusty as fuck in PHP:

--- login.php
+++ login.php
@@ -66,7 +66,7 @@ if ( isset($_GET['g']) && $_GET['g'] == 'csrf' ) {
     }
     
     // > Validar respuesta de App.
-    $response = json_decode($raw_response);
+    $response = json_decode($raw_response, TRUE);
     if ( $response == NULL || ! isset($response['k']) || ! isset($response['h']) ) {
         if ( isset($response['error']) ) {
             redirect_fail();
